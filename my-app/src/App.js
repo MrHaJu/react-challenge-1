113a338380af2ebb1576c2ed87d26d25b2555672
MrHaJu <>
2023-11-01 12:51:57 +0000

    initial commit

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,13 @@
 import css from './App.module.css';
+import Sidebar from "./components/Sidebar";
+
 
 function App() {
   return (
     <div className={css.App}>
-      {/* Add your components here */}
-    </div>
+    {/* Add your components here */}
+    <Sidebar />
+  </div>
   );
 }
 
